3ec5026f9c592f131efc9555cc7b1b981b1ae9e1
Navyasri Vadde <navyasri.vadde1@wipro.com>
2017-11-02 16:40:52 +0530

    update API issue fixed

--- src/main/java/com/iemr/mmu/repo/registrar/BeneficiaryDemographicAdditionalRepo.java
+++ src/main/java/com/iemr/mmu/repo/registrar/BeneficiaryDemographicAdditionalRepo.java
@@ -1,5 +1,6 @@
 package com.iemr.mmu.repo.registrar;
 
+import java.util.List;
 
 import javax.transaction.Transactional;
 
@@ -8,7 +9,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-;
+
+import com.iemr.mmu.data.nurse.BeneficiaryVisitDetail;
 import com.iemr.mmu.data.registrar.BeneficiaryDemographicAdditional;
 
 @Repository
