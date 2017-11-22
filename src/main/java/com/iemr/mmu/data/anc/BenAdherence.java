ed4e376b4d4ec28fc43aeb5620d96820f5e848a0
Neeraj Kumar (INDIA - HCIT) <NE298657@wipro.com>
2017-11-22 16:51:36 +0530

    ben adherence

---
+++ src/main/java/com/iemr/mmu/data/anc/BenAdherence.java
@@ -0,0 +1,209 @@
+package com.iemr.mmu.data.anc;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.google.gson.annotations.Expose;
+
+@Entity
+@Table(name = "t_BenAdherence")
+public class BenAdherence {
+
+	@Id
+	@GeneratedValue
+	@Expose
+	@Column(name = "ID")
+	private Long ID;
+	@Expose
+	@Column(name = "BeneficiaryRegID")
+	private Long beneficiaryRegID;
+	@Expose
+	@Column(name = "BenVisitID")
+	private Long benVisitID;
+	@Expose
+	@Column(name = "BenVisitID")
+	private Integer providerServiceMapID;
+	@Expose
+	@Column(name = "ToDrugs")
+	private Boolean toDrugs;
+	@Expose
+	@Column(name = "DrugReason")
+	private String drugReason;
+	@Expose
+	@Column(name = "ToReferral")
+	private Boolean toReferral;
+	@Expose
+	@Column(name = "ReferralReason")
+	private String referralReason;
+	@Expose
+	@Column(name = "Progress")
+	private String progress;
+	@Expose
+	@Column(name = "Deleted")
+	private Boolean deleted;
+	@Expose
+	@Column(name = "Processed")
+	private String processed;
+	@Expose
+	@Column(name = "Processed", insertable = false)
+	private String createdBy;
+	@Expose
+	@Column(name = "CreatedDate", insertable = false, updatable = false)
+	private Timestamp createdDate;
+	@Expose
+	@Column(name = "ModifiedBy")
+	private String modifiedBy;
+	@Expose
+	@Column(name = "LastModDate", insertable = false)
+	private Timestamp lastModDate;
+
+	public BenAdherence() {
+	}
+
+	public BenAdherence(Long iD, Long beneficiaryRegID, Long benVisitID, Integer providerServiceMapID, Boolean toDrugs,
+			String drugReason, Boolean toReferral, String referralReason, String progress, Boolean deleted,
+			String processed, String createdBy, Timestamp createdDate, String modifiedBy, Timestamp lastModDate) {
+		super();
+		ID = iD;
+		this.beneficiaryRegID = beneficiaryRegID;
+		this.benVisitID = benVisitID;
+		this.providerServiceMapID = providerServiceMapID;
+		this.toDrugs = toDrugs;
+		this.drugReason = drugReason;
+		this.toReferral = toReferral;
+		this.referralReason = referralReason;
+		this.progress = progress;
+		this.deleted = deleted;
+		this.processed = processed;
+		this.createdBy = createdBy;
+		this.createdDate = createdDate;
+		this.modifiedBy = modifiedBy;
+		this.lastModDate = lastModDate;
+	}
+
+	public Long getID() {
+		return ID;
+	}
+
+	public void setID(Long iD) {
+		ID = iD;
+	}
+
+	public Long getBeneficiaryRegID() {
+		return beneficiaryRegID;
+	}
+
+	public void setBeneficiaryRegID(Long beneficiaryRegID) {
+		this.beneficiaryRegID = beneficiaryRegID;
+	}
+
+	public Long getBenVisitID() {
+		return benVisitID;
+	}
+
+	public void setBenVisitID(Long benVisitID) {
+		this.benVisitID = benVisitID;
+	}
+
+	public Integer getProviderServiceMapID() {
+		return providerServiceMapID;
+	}
+
+	public void setProviderServiceMapID(Integer providerServiceMapID) {
+		this.providerServiceMapID = providerServiceMapID;
+	}
+
+	public Boolean getToDrugs() {
+		return toDrugs;
+	}
+
+	public void setToDrugs(Boolean toDrugs) {
+		this.toDrugs = toDrugs;
+	}
+
+	public String getDrugReason() {
+		return drugReason;
+	}
+
+	public void setDrugReason(String drugReason) {
+		this.drugReason = drugReason;
+	}
+
+	public Boolean getToReferral() {
+		return toReferral;
+	}
+
+	public void setToReferral(Boolean toReferral) {
+		this.toReferral = toReferral;
+	}
+
+	public String getReferralReason() {
+		return referralReason;
+	}
+
+	public void setReferralReason(String referralReason) {
+		this.referralReason = referralReason;
+	}
+
+	public String getProgress() {
+		return progress;
+	}
+
+	public void setProgress(String progress) {
+		this.progress = progress;
+	}
+
+	public Boolean getDeleted() {
+		return deleted;
+	}
+
+	public void setDeleted(Boolean deleted) {
+		this.deleted = deleted;
+	}
+
+	public String getProcessed() {
+		return processed;
+	}
+
+	public void setProcessed(String processed) {
+		this.processed = processed;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Timestamp getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Timestamp createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public String getModifiedBy() {
+		return modifiedBy;
+	}
+
+	public void setModifiedBy(String modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+
+	public Timestamp getLastModDate() {
+		return lastModDate;
+	}
+
+	public void setLastModDate(Timestamp lastModDate) {
+		this.lastModDate = lastModDate;
+	}
+
+}
