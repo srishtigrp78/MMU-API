06af1a72c2bb980f30014eb64517c6d12f320f9f
Neeraj Kumar (INDIA - HCIT) <NE298657@wipro.com>
2018-01-08 18:44:56 +0530

    registration bug

--- src/main/java/com/iemr/mmu/repo/doctor/DocWorkListRepo.java
+++ src/main/java/com/iemr/mmu/repo/doctor/DocWorkListRepo.java
@@ -14,7 +14,7 @@ public interface DocWorkListRepo extends CrudRepository<DocWorkList, Long> {
 			+ " UPPER( concat(IFNULL(firstName, ''), ' ',IFNULL(lastName,''))) as benName, "
 			+ " Date(dob), genderID, genderName, benVisitID, visitNo, visitFlowStatusFlag, "
 			+ " visitCategory, fatherName, districtName, villageName, phoneNo, DATE_FORMAT(visitCreatedDate, '%d-%m-%Y')  from DocWorkList  "
-			+ " where visitFlowStatusFlag in( 'N', 'D') and Date(visitCreatedDate) = curdate() ")
+			+ " where (visitFlowStatusFlag  = 'N') or  (visitFlowStatusFlag  = 'D' and Date(visitCreatedDate) = curdate()) ")
 	public List<Object[]> getDocWorkList();
 	
 	@Query("SELECT DISTINCT beneficiaryRegID, beneficiaryID, "
