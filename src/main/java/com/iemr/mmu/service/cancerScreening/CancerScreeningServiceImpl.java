6ad07e3a7d30c0f2f69c3cd74727146ba467405c
Neeraj Kumar (INDIA - HCIT) <NE298657@wipro.com>
2018-01-18 15:12:02 +0530

    cancer screening nurse data saving

--- src/main/java/com/iemr/mmu/service/cancerScreening/CancerScreeningServiceImpl.java
+++ src/main/java/com/iemr/mmu/service/cancerScreening/CancerScreeningServiceImpl.java
@@ -1,10 +1,17 @@
 package com.iemr.mmu.service.cancerScreening;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.google.gson.JsonObject;
+import com.iemr.mmu.data.nurse.BenCancerVitalDetail;
 import com.iemr.mmu.data.nurse.BenFamilyCancerHistory;
+import com.iemr.mmu.data.nurse.BenObstetricCancerHistory;
+import com.iemr.mmu.data.nurse.BenPersonalCancerDietHistory;
+import com.iemr.mmu.data.nurse.BenPersonalCancerHistory;
 import com.iemr.mmu.data.nurse.BeneficiaryVisitDetail;
 import com.iemr.mmu.service.nurse.NurseServiceImpl;
 import com.iemr.utils.mapper.InputMapper;
@@ -20,44 +27,137 @@ public class CancerScreeningServiceImpl implements CancerScreeningService {
 	}
 
 	public void saveCancerScreeningNurseData(JsonObject requestOBJ) {
-		System.out.println("hi...");
+		// check if visit details data is not null
 		if (requestOBJ != null && requestOBJ.has("visitDetails") && !requestOBJ.get("visitDetails").isJsonNull()) {
-			BeneficiaryVisitDetail benVisitDetailsOBJ = InputMapper.gson().fromJson(requestOBJ.get("visitDetails"),
-					BeneficiaryVisitDetail.class);
-			Long benVisitID = nurseServiceImpl.saveBeneficiaryVisitDetails(benVisitDetailsOBJ);
-			System.out.println("hi...");
+			// Call method to save visit details data
+			Long benVisitID = saveBenVisitDetails(requestOBJ);
+			// check if visit details data saved successfully
 			if (benVisitID != null && benVisitID > 0) {
-				if (requestOBJ != null && requestOBJ.has("historyDetails")
-						&& !requestOBJ.get("historyDetails").isJsonNull()) {
-					JsonObject historyOBJ = requestOBJ.getAsJsonObject("historyDetails");
-					System.out.println("hi...");
-					if (historyOBJ != null && historyOBJ.has("familyHistory")
-							&& !historyOBJ.get("familyHistory").isJsonNull()) {
-						System.out.println("hi...");
-						JsonObject familyHistoryOBJ = historyOBJ.getAsJsonObject("familyHistory");
-						if (familyHistoryOBJ != null && familyHistoryOBJ.has("diseases")
-								&& !familyHistoryOBJ.get("diseases").isJsonNull()) {
-							System.out.println("hi...");
-							BenFamilyCancerHistory[] benFamilyCancerHistoryArray = InputMapper.gson().fromJson(
-									familyHistoryOBJ.get("diseases").getAsJsonArray(), BenFamilyCancerHistory[].class);
-							System.out.println("hi...");
-						} else {
-							// familyHistoryOBJ is null or diseases are not
-							// there in familyHistoryOBJ or null
-						}
-					} else {
-						// history Obj is null or family history history is null
-						// or not there in history obj !!!
-					}
+				// call method to save history data
+				Long historySaveSuccessFlag = saveBenHistoryDetails(requestOBJ, benVisitID);
+				// call method to save vitals data
+				Long vitalSaveSuccessFlag = saveBenVitalsDetails(requestOBJ, benVisitID);
 
-				} else {
-					// History Object is not there in request object !!!
-				}
 			} else {
-				// Error in visit details data saving....so no visit id created
-				// for beneficiary !!!
+				// Error in visit details saving or it is null
 			}
 		}
-		System.out.println("hi...");
+	}
+
+	/**
+	 * 
+	 * @param requestOBJ
+	 * @return success or failure flag for visitDetails data saving
+	 */
+	public Long saveBenVisitDetails(JsonObject requestOBJ) {
+		BeneficiaryVisitDetail benVisitDetailsOBJ = InputMapper.gson().fromJson(requestOBJ.get("visitDetails"),
+				BeneficiaryVisitDetail.class);
+		Long benVisitID = nurseServiceImpl.saveBeneficiaryVisitDetails(benVisitDetailsOBJ);
+		return benVisitID;
+	}
+
+	/**
+	 * 
+	 * @param requestOBJ
+	 * @param benVisitID
+	 * @return success or failure flag for history data saving
+	 */
+	public Long saveBenHistoryDetails(JsonObject requestOBJ, Long benVisitID) {
+		if (requestOBJ != null && requestOBJ.has("historyDetails") && !requestOBJ.get("historyDetails").isJsonNull()) {
+			JsonObject historyOBJ = requestOBJ.getAsJsonObject("historyDetails");
+			System.out.println("hi...");
+			if (historyOBJ != null && historyOBJ.has("familyHistory")
+					&& !historyOBJ.get("familyHistory").isJsonNull()) {
+				System.out.println("hi...");
+				JsonObject familyHistoryOBJ = historyOBJ.getAsJsonObject("familyHistory");
+				if (familyHistoryOBJ != null && familyHistoryOBJ.has("diseases")
+						&& !familyHistoryOBJ.get("diseases").isJsonNull()) {
+					System.out.println("hi...");
+					BenFamilyCancerHistory[] benFamilyCancerHistoryArray = InputMapper.gson().fromJson(
+							familyHistoryOBJ.get("diseases").getAsJsonArray(), BenFamilyCancerHistory[].class);
+					List<BenFamilyCancerHistory> benFamilyCancerHistoryList = Arrays
+							.asList(benFamilyCancerHistoryArray);
+					if (benFamilyCancerHistoryArray != null && benFamilyCancerHistoryList.size() > 0) {
+						for (BenFamilyCancerHistory obj : benFamilyCancerHistoryArray) {
+							obj.setBenVisitID(benVisitID);
+						}
+						Integer benFamilyHistoryDataSavingSuccessFlag = nurseServiceImpl
+								.saveBenFamilyCancerHistory(benFamilyCancerHistoryList);
+
+					} else {
+						// family cancer history data is null for
+						// beneficiary !!!
+					}
+					System.out.println("hi...");
+				} else {
+					// familyHistoryOBJ is null or diseases are not
+					// there in familyHistoryOBJ or null
+				}
+			} else {
+				// history Obj is null or family history history is null
+				// or not there in history obj !!!
+			}
+			if (historyOBJ != null && historyOBJ.has("personalHistory")
+					&& !historyOBJ.get("personalHistory").isJsonNull()) {
+				BenPersonalCancerHistory benPersonalCancerHistory = InputMapper.gson()
+						.fromJson(historyOBJ.get("personalHistory"), BenPersonalCancerHistory.class);
+
+				BenPersonalCancerDietHistory benPersonalCancerDietHistory = InputMapper.gson()
+						.fromJson(historyOBJ.get("personalHistory"), BenPersonalCancerDietHistory.class);
+
+				if (benPersonalCancerHistory != null && benPersonalCancerDietHistory != null) {
+
+					benPersonalCancerHistory.setBenVisitID(benVisitID);
+					benPersonalCancerDietHistory.setBenVisitID(benVisitID);
+
+					Long benPersonalHistorySaveSuccessFlag = nurseServiceImpl
+							.saveBenPersonalCancerHistory(benPersonalCancerHistory);
+					Long benPersonalDietHistorySaveSuccessFlag = nurseServiceImpl
+							.saveBenPersonalCancerDietHistory(benPersonalCancerDietHistory);
+				}
+			} else {
+				// ben personal history data not there !!!
+			}
+			if (historyOBJ != null && historyOBJ.has("pastObstetricHistory")
+					&& !historyOBJ.get("pastObstetricHistory").isJsonNull()) {
+				BenObstetricCancerHistory benObstetricCancerHistory = InputMapper.gson()
+						.fromJson(historyOBJ.get("pastObstetricHistory"), BenObstetricCancerHistory.class);
+
+				benObstetricCancerHistory.setBenVisitID(benVisitID);
+				Long benObstetricSaveSuccessFlag = nurseServiceImpl
+						.saveBenObstetricCancerHistory(benObstetricCancerHistory);
+			} else {
+				// ben obstetric history data not there !!!
+			}
+
+		} else {
+			// History Object is not there in request object !!!
+		}
+		return null;
+	}
+
+	public Long saveBenFamilyHistoryDetails() {
+		return null;
+	}
+
+	/**
+	 * 
+	 * @param requestOBJ
+	 * @param benVisitID
+	 * @return success or failure flag for vitals data saving
+	 */
+	public Long saveBenVitalsDetails(JsonObject requestOBJ, Long benVisitID) {
+		Long benVitalSaveSuccessFlag = null;
+		if (requestOBJ != null && requestOBJ.has("vitalsDetails") && !requestOBJ.get("vitalsDetails").isJsonNull()) {
+			BenCancerVitalDetail benCancerVitalDetail = InputMapper.gson().fromJson(requestOBJ.get("vitalsDetails"),
+					BenCancerVitalDetail.class);
+			if (benCancerVitalDetail != null) {
+				benCancerVitalDetail.setBenVisitID(benVisitID);
+				benVitalSaveSuccessFlag = nurseServiceImpl.saveBenVitalDetail(benCancerVitalDetail);
+			}
+		} else {
+			// ben vitals data is not there !!!
+		}
+		return benVitalSaveSuccessFlag;
 	}
 }
