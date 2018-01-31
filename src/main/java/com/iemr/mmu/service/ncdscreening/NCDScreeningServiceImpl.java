6f007e2b5e96a9a2eaf9e5ebea353971abc31476
Neeraj Kumar (INDIA - HCIT) <NE298657@wipro.com>
2018-01-31 10:43:50 +0530

    ncd screening serviceIMPL

--- src/main/java/com/iemr/mmu/service/ncdscreening/NCDScreeningServiceImpl.java
+++ src/main/java/com/iemr/mmu/service/ncdscreening/NCDScreeningServiceImpl.java
@@ -7,15 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.iemr.mmu.data.ncdScreening.NCDScreening;
 import com.iemr.mmu.data.nurse.BenAnthropometryDetail;
 import com.iemr.mmu.data.nurse.BenPhysicalVitalDetail;
 import com.iemr.mmu.data.nurse.BeneficiaryVisitDetail;
-import com.iemr.mmu.repo.nurse.ncdscreening.NCDScreeningRepo;
 import com.iemr.mmu.service.anc.ANCNurseServiceImpl;
 import com.iemr.mmu.service.nurse.NurseServiceImpl;
 import com.iemr.mmu.utils.exception.IEMRException;
@@ -28,7 +25,6 @@ public class NCDScreeningServiceImpl implements NCDScreeningService {
 	private NurseServiceImpl nurseServiceImpl;
 	private ANCNurseServiceImpl ancNurseServiceImpl;
 
-
 	@Autowired
 	public void setNurseServiceImpl(NurseServiceImpl nurseServiceImpl) {
 		this.nurseServiceImpl = nurseServiceImpl;
@@ -38,7 +34,7 @@ public class NCDScreeningServiceImpl implements NCDScreeningService {
 	public void setAncServiceImpl(ANCNurseServiceImpl ancNurseServiceImpl) {
 		this.ancNurseServiceImpl = ancNurseServiceImpl;
 	}
-	
+
 	@Autowired
 	public void setNcdScreeningNurseServiceImpl(NCDScreeningNurseServiceImpl ncdScreeningNurseServiceImpl) {
 		this.ncdScreeningNurseServiceImpl = ncdScreeningNurseServiceImpl;
@@ -48,89 +44,89 @@ public class NCDScreeningServiceImpl implements NCDScreeningService {
 	@Transactional(rollbackFor = Exception.class)
 	public Integer saveNCDScreeningNurseData(JsonObject jsonObject) throws Exception {
 
-		JsonElement visitDetails = jsonObject.get("visitDetails");
-		JsonElement ncdScreeningDetails = jsonObject.get("ncdScreeningDetails");
-
-		BeneficiaryVisitDetail beneficiaryVisitDetail = InputMapper.gson().fromJson(visitDetails,
-				BeneficiaryVisitDetail.class);
-		
-		NCDScreening ncdScreening = InputMapper.gson().fromJson(ncdScreeningDetails, NCDScreening.class);
-
-		Long visitID = nurseServiceImpl.saveBeneficiaryVisitDetails(beneficiaryVisitDetail);
 		Integer result = null;
 		Integer updateStatus = null;
+		Long labTestOrderID = null;
 
-		if (null != visitID) {
-			
-			Long vitalSuccessFlag = saveNCDScreeningVitalDetails(jsonObject, visitID);
-			Long saveNCDScreeningDetails = null;
-			if(null != ncdScreening){
-				ncdScreening.setBenVisitID(visitID);
-				saveNCDScreeningDetails = ncdScreeningNurseServiceImpl.saveNCDScreeningDetails(ncdScreening);
-			}
+		if (jsonObject != null && jsonObject.has("visitDetails") && jsonObject.has("ncdScreeningDetails")) {
+			JsonElement visitDetails = jsonObject.get("visitDetails");
+			JsonElement ncdScreeningDetails = jsonObject.get("ncdScreeningDetails");
 
-			Long prescriptionID = nurseServiceImpl.saveBeneficiaryPrescription(jsonObject);
+			BeneficiaryVisitDetail beneficiaryVisitDetail = InputMapper.gson().fromJson(visitDetails,
+					BeneficiaryVisitDetail.class);
 
-			Long labTestOrderID = null;
+			NCDScreening ncdScreening = InputMapper.gson().fromJson(ncdScreeningDetails, NCDScreening.class);
 
-			if (prescriptionID != null && prescriptionID > 0) {
+			Long visitID = nurseServiceImpl.saveBeneficiaryVisitDetails(beneficiaryVisitDetail);
 
-				labTestOrderID = nurseServiceImpl.saveBeneficiaryLabTestOrderDetails(jsonObject, prescriptionID);
+			if (null != visitID) {
 
-			}
-			
-			if (null != vitalSuccessFlag && null != saveNCDScreeningDetails) {
-				updateStatus = nurseServiceImpl.updateBeneficiaryStatus('N',
-						beneficiaryVisitDetail.getBeneficiaryRegID());
-				result = 1;
-			}
+				Long vitalSuccessFlag = saveNCDScreeningVitalDetails(jsonObject, visitID);
+				Long saveNCDScreeningDetails = null;
+				if (null != ncdScreening) {
+					ncdScreening.setBenVisitID(visitID);
+					saveNCDScreeningDetails = ncdScreeningNurseServiceImpl.saveNCDScreeningDetails(ncdScreening);
+				}
 
-			if (null == result || null == updateStatus)
+				Long prescriptionID = nurseServiceImpl.saveBeneficiaryPrescription(jsonObject);
+
+				if (prescriptionID != null && prescriptionID > 0) {
+
+					labTestOrderID = nurseServiceImpl.saveBeneficiaryLabTestOrderDetails(jsonObject, prescriptionID);
+
+				}
+
+				if (null != vitalSuccessFlag && null != saveNCDScreeningDetails) {
+					updateStatus = nurseServiceImpl.updateBeneficiaryStatus('N',
+							beneficiaryVisitDetail.getBeneficiaryRegID());
+					result = 1;
+				}
+
+				if (null == result || null == updateStatus)
+					throw new Exception("Insertion Error");
+			} else {
 				throw new Exception("Insertion Error");
-		} else {
-			throw new Exception("Insertion Error");
+			}
+
 		}
 		return result;
 	}
 
 	public Long saveNCDScreeningVitalDetails(JsonObject jsonObject, Long benVisitID) {
-		
+
 		Long vitalSuccessFlag = null;
 		JsonElement ncdScreeningDetails = jsonObject.get("ncdScreeningDetails");
-		
+
 		BenAnthropometryDetail anthropometryDetail = null;
 		BenPhysicalVitalDetail physicalVitalDetail = null;
 		try {
-			anthropometryDetail = InputMapper.gson().fromJson(ncdScreeningDetails,
-					BenAnthropometryDetail.class);
-			
-			physicalVitalDetail = InputMapper.gson().fromJson(ncdScreeningDetails,
-					BenPhysicalVitalDetail.class);
+			anthropometryDetail = InputMapper.gson().fromJson(ncdScreeningDetails, BenAnthropometryDetail.class);
+
+			physicalVitalDetail = InputMapper.gson().fromJson(ncdScreeningDetails, BenPhysicalVitalDetail.class);
 		} catch (IEMRException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		Long saveAnthropometryDetail = null;
-		if(null != anthropometryDetail){
+		if (null != anthropometryDetail) {
 			anthropometryDetail.setBenVisitID(benVisitID);
-			saveAnthropometryDetail = nurseServiceImpl
-					.saveBeneficiaryPhysicalAnthropometryDetails(anthropometryDetail);
+			saveAnthropometryDetail = nurseServiceImpl.saveBeneficiaryPhysicalAnthropometryDetails(anthropometryDetail);
 		}
 		Long savePhysicalVitalDetails = null;
-		if(null != physicalVitalDetail){
+		if (null != physicalVitalDetail) {
 			physicalVitalDetail.setBenVisitID(benVisitID);
 			savePhysicalVitalDetails = nurseServiceImpl.saveBeneficiaryPhysicalVitalDetails(physicalVitalDetail);
 		}
-		
-		if((null != saveAnthropometryDetail && saveAnthropometryDetail>0) && (null != savePhysicalVitalDetails && savePhysicalVitalDetails>0)){
+
+		if ((null != saveAnthropometryDetail && saveAnthropometryDetail > 0)
+				&& (null != savePhysicalVitalDetails && savePhysicalVitalDetails > 0)) {
 			vitalSuccessFlag = saveAnthropometryDetail;
 		}
-		
+
 		return vitalSuccessFlag;
 	}
-	
-	
+
 	@Override
 	@Transactional(rollbackFor = Exception.class)
 	public Integer updateNurseNCDScreeningDetails(JsonObject jsonObject) throws Exception {
@@ -163,9 +159,9 @@ public class NCDScreeningServiceImpl implements NCDScreeningService {
 		String anthropometryDetails = nurseServiceImpl.getBeneficiaryPhysicalAnthropometryDetails(beneficiaryRegID,
 				benVisitID);
 		String vitalDetails = nurseServiceImpl.getBeneficiaryPhysicalVitalDetails(beneficiaryRegID, benVisitID);
-	
+
 		Map<String, Object> res = new HashMap<String, Object>();
-	
+
 		if (ncdScreeningDetails != null && anthropometryDetails != null && vitalDetails != null) {
 			res.put("ncdScreeningDetails", ncdScreeningDetails);
 			res.put("anthropometryDetails", anthropometryDetails);
