2db67957b5ccd6ac5b1785d85a2ebdf4fc83e5f2
NE298657 <NE298657@wipro.com>
2018-03-20 14:47:05 +0530

    ben flow

--- src/main/java/com/iemr/mmu/service/benFlowStatus/CommonBenStatusFlowServiceImpl.java
+++ src/main/java/com/iemr/mmu/service/benFlowStatus/CommonBenStatusFlowServiceImpl.java
@@ -24,7 +24,7 @@ public class CommonBenStatusFlowServiceImpl implements CommonBenStatusFlowServic
 	@Autowired
 	public void setCommonNurseServiceImpl(CommonNurseServiceImpl commonNurseServiceImpl) {
 		this.commonNurseServiceImpl = commonNurseServiceImpl;
-	}
+	} 
 
 	@Autowired
 	public void setBeneficiaryFlowStatusRepo(BeneficiaryFlowStatusRepo beneficiaryFlowStatusRepo) {
