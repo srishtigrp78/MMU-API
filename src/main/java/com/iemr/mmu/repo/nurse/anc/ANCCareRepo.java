0caf2049ad5aeb9e05648600ff52e88fb0856569
Navyasri Vadde <navyasri.vadde1@wipro.com>
2018-04-23 12:30:31 +0530

    Anc care DB changes

--- src/main/java/com/iemr/mmu/repo/nurse/anc/ANCCareRepo.java
+++ src/main/java/com/iemr/mmu/repo/nurse/anc/ANCCareRepo.java
@@ -16,9 +16,16 @@ import com.iemr.mmu.data.anc.ANCCareDetails;
 @Repository
 public interface ANCCareRepo extends CrudRepository<ANCCareDetails, Long>{
 
-	@Query(" SELECT ID, beneficiaryRegID, benVisitID, providerServiceMapID, visitNo, comolaintType, duration, description, aNCRegistrationDate,"
+	/*@Query(" SELECT ID, beneficiaryRegID, benVisitID, providerServiceMapID, visitNo, comolaintType, duration, description, aNCRegistrationDate,"
 			+ " aNCVisitNumber, lastMenstrualPeriod_LMP, gestationalAgeOrPeriodofAmenorrhea_POA, trimesterNumber, expectedDateofDelivery, "
 			+ "primiGravida, obstetricFormula, gravida_G, termDeliveries_T, pretermDeliveries_P, abortions_A, livebirths_L, bloodGroup "
+			+ "from ANCCareDetails ba WHERE ba.beneficiaryRegID = :benRegID AND ba.benVisitID = :benVisitID ")
+	public ArrayList<Object[]> getANCCareDetails(@Param("benRegID") Long benRegID,
+			@Param("benVisitID") Long benVisitID);*/
+
+	@Query(" SELECT ID, beneficiaryRegID, benVisitID, providerServiceMapID, visitNo, comolaintType, duration, description, "
+			+ " lastMenstrualPeriod_LMP, gestationalAgeOrPeriodofAmenorrhea_POA, trimesterNumber, expectedDateofDelivery, "
+			+ "primiGravida, gravida_G, termDeliveries_T, pretermDeliveries_P, abortions_A, livebirths_L, bloodGroup "
 			+ "from ANCCareDetails ba WHERE ba.beneficiaryRegID = :benRegID AND ba.benVisitID = :benVisitID ")
 	public ArrayList<Object[]> getANCCareDetails(@Param("benRegID") Long benRegID,
 			@Param("benVisitID") Long benVisitID);
@@ -30,24 +37,21 @@ public interface ANCCareRepo extends CrudRepository<ANCCareDetails, Long>{
 	
 	@Transactional
 	@Modifying
-	@Query("update ANCCareDetails set comolaintType=:comolaintType, duration=:duration, description=:description, aNCRegistrationDate=:aNCRegistrationDate,"
-			+ " aNCVisitNumber=:aNCVisitNumber, lastMenstrualPeriod_LMP=:lastMenstrualPeriod_LMP, "
+	@Query("update ANCCareDetails set comolaintType=:comolaintType, duration=:duration, description=:description, "
+			+ "lastMenstrualPeriod_LMP=:lastMenstrualPeriod_LMP, "
 			+ "gestationalAgeOrPeriodofAmenorrhea_POA=:gestationalAgeOrPeriodofAmenorrhea_POA,"
 			+ "trimesterNumber=:trimesterNumber,  expectedDateofDelivery=:expectedDateofDelivery, primiGravida=:primiGravida, "
-			+ "obstetricFormula=:obstetricFormula, gravida_G=:gravida_G, termDeliveries_T=:termDeliveries_T, pretermDeliveries_P=:pretermDeliveries_P,"
+			+ "gravida_G=:gravida_G, termDeliveries_T=:termDeliveries_T, pretermDeliveries_P=:pretermDeliveries_P,"
 			+ "abortions_A=:abortions_A,livebirths_L=:livebirths_L, bloodGroup=:bloodGroup, modifiedBy=:modifiedBy, processed=:processed"
 			+ " where benVisitID=:benVisitID AND beneficiaryRegID=:beneficiaryRegID")
 	public int updateANCCareDetails(@Param("comolaintType") String comolaintType,
 			@Param("duration") String duration,
 			@Param("description") String description,
-			@Param("aNCRegistrationDate") Date aNCRegistrationDate,
-			@Param("aNCVisitNumber") Short aNCVisitNumber,
 			@Param("lastMenstrualPeriod_LMP") Date lastMenstrualPeriod_LMP,
 			@Param("gestationalAgeOrPeriodofAmenorrhea_POA") Short gestationalAgeOrPeriodofAmenorrhea_POA,
 			@Param("trimesterNumber") Short trimesterNumber,
 			@Param("expectedDateofDelivery") Date expectedDateofDelivery,
 			@Param("primiGravida") Boolean primiGravida,
-			@Param("obstetricFormula") String obstetricFormula,
 			@Param("gravida_G") Short gravida_G,
 			@Param("termDeliveries_T") Short termDeliveries_T,
 			@Param("pretermDeliveries_P") Short pretermDeliveries_P,
