a3d623c45ef4a90066c54e2518935a48a5e81a99
Anjana <anjana.j28@wipro.com>
2023-07-21 14:13:34 +0530

    clean up,buildfix,encryption removal

--- src/main/java/com/iemr/mmu/service/reports/ReportCheckPostImpl.java
+++ src/main/java/com/iemr/mmu/service/reports/ReportCheckPostImpl.java
@@ -21,15 +21,9 @@
 */
 package com.iemr.mmu.service.reports;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.sql.Timestamp;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-import org.apache.commons.collections.ListUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -38,7 +32,6 @@ import com.iemr.mmu.data.reports.Report_ANC;
 import com.iemr.mmu.data.reports.Report_ANCHighRisk;
 import com.iemr.mmu.data.reports.Report_ChildrenCases;
 import com.iemr.mmu.data.reports.Report_LabTestsResult;
-import com.iemr.mmu.data.reports.Report_ModifiedAnc;
 import com.iemr.mmu.data.reports.Report_PatientAttended;
 import com.iemr.mmu.data.reports.Report_PatientInfo;
 import com.iemr.mmu.data.reports.Report_TestConducted;
@@ -97,8 +90,6 @@ public class ReportCheckPostImpl implements ReportCheckPost {
 						request.getProviderServiceMapID());
 				break;
 
-			
-
 			default:// default statement
 			}
 		} else
@@ -109,8 +100,6 @@ public class ReportCheckPostImpl implements ReportCheckPost {
 	private String report_PatientAttended(Timestamp fromDate, Timestamp toDate, Integer vanID, Integer psmID)
 			throws Exception {
 		ArrayList<Report_PatientAttended> report_PA = new ArrayList<>();
-
-		// Gson gson = new GsonBuilder().serializeNulls().create();
 
 		if (fromDate == null || toDate == null || vanID == null || psmID == null)
 			throw new Exception("Some parameter/parameters is/are missing.");
@@ -127,8 +116,6 @@ public class ReportCheckPostImpl implements ReportCheckPost {
 	private String report_TestConducted(Timestamp fromDate, Timestamp toDate, Integer vanID, Integer psmID)
 			throws Exception {
 		ArrayList<Report_TestConducted> report_TC = new ArrayList<>();
-
-		// Gson gson = new GsonBuilder().serializeNulls().create();
 
 		if (fromDate == null || toDate == null || vanID == null || psmID == null)
 			throw new Exception("Some parameter/parameters is/are missing.");
@@ -164,8 +151,6 @@ public class ReportCheckPostImpl implements ReportCheckPost {
 			throws Exception {
 		ArrayList<Report_PatientInfo> report_PI = new ArrayList<>();
 
-		// Gson gson = new GsonBuilder().serializeNulls().create();
-
 		if (fromDate == null || toDate == null || vanID == null || psmID == null)
 			throw new Exception("Some parameter/parameters is/are missing.");
 		else {
@@ -182,8 +167,6 @@ public class ReportCheckPostImpl implements ReportCheckPost {
 			throws Exception {
 		ArrayList<Report_ChildrenCases> report_PI = new ArrayList<>();
 
-		// Gson gson = new GsonBuilder().serializeNulls().create();
-
 		if (fromDate == null || toDate == null || vanID == null || psmID == null)
 			throw new Exception("Some parameter/parameters is/are missing.");
 		else {
@@ -198,8 +181,6 @@ public class ReportCheckPostImpl implements ReportCheckPost {
 
 	private String report_ANC(Timestamp fromDate, Timestamp toDate, Integer vanID, Integer psmID) throws Exception {
 		ArrayList<Report_ANC> report_ANC = new ArrayList<>();
-
-		// Gson gson = new GsonBuilder().serializeNulls().create();
 
 		if (fromDate == null || toDate == null || vanID == null || psmID == null)
 			throw new Exception("Some parameter/parameters is/are missing.");
@@ -217,8 +198,6 @@ public class ReportCheckPostImpl implements ReportCheckPost {
 			throws Exception {
 		ArrayList<Report_ANCHighRisk> report_ANCHighRisk = new ArrayList<>();
 
-		// Gson gson = new GsonBuilder().serializeNulls().create();
-
 		if (fromDate == null || toDate == null || vanID == null || psmID == null)
 			throw new Exception("Some parameter/parameters is/are missing.");
 		else {
@@ -231,6 +210,4 @@ public class ReportCheckPostImpl implements ReportCheckPost {
 		return OutputMapper.gson().toJson(report_ANCHighRisk);
 	}
 
-	
-	
 }
--- src/main/java/com/iemr/mmu/utils/IEMRApplBeans.java
+++ src/main/java/com/iemr/mmu/utils/IEMRApplBeans.java
@@ -27,13 +27,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.session.data.redis.config.annotation.web.http.RedisHttpSessionConfiguration;
 
 import com.iemr.mmu.utils.config.ConfigProperties;
 import com.iemr.mmu.utils.gateway.email.EmailService;
 import com.iemr.mmu.utils.gateway.email.GenericEmailServiceImpl;
-import com.iemr.mmu.utils.km.KMService;
-import com.iemr.mmu.utils.km.openkm.OpenKMServiceImpl;
 import com.iemr.mmu.utils.redis.RedisStorage;
 import com.iemr.mmu.utils.sessionobject.SessionObject;
 import com.iemr.mmu.utils.validator.Validator;
@@ -43,12 +40,6 @@ public class IEMRApplBeans {
 
 	private @Value("${spring.redis.host}") String redisHost;
 	private @Value("${spring.redis.port}") int redisPort;
-
-	@Bean
-	public KMService getOpenKMService() {
-		KMService kmService = new OpenKMServiceImpl();
-		return kmService;
-	}
 
 	@Bean
 	public Validator getVaidator() {
@@ -83,41 +74,8 @@ public class IEMRApplBeans {
 		return new RedisStorage();
 	}
 
-	// @Beanss
-	// public RedisConnection redisConnection()
-	// {
-	// return new RedisConnection();
-	// }
-
-	// @Configuration
-	// @EnableRedisHttpSession
-	// public class Config
-	// {
-
 	@Bean
 	public LettuceConnectionFactory connectionFactory() {
 		return new LettuceConnectionFactory(redisHost, redisPort);
 	}
-	// }
-
-//	@Bean
-//	public RedisHttpSessionConfiguration redisSession() {
-//		return new RedisHttpSessionConfiguration();
-//	}
-
-	// @Bean
-	// public HTTPRequestInterceptor myInterceptor()
-	// {
-	// return new HTTPRequestInterceptor();
-	// }
-	// @Bean
-	// public KMService getOpenKMService()
-	// {
-	// KMService kmService = new OpenKMServiceImpl();
-	// return kmService;
-	// }
-
-	// public static void main(String[] args) {
-	// SpringApplication.run(CommonMain.class, args);
-	// }
 }
