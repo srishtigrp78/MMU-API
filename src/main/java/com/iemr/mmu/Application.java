55d64486be422533b1ec045a5592d9337285bd1d
Ravi Shanigarapu <ravi.shanigarapu@wipro.com>
2024-02-02 11:21:58 +0530

    removed swagger config file and removed commented code in Application file

--- src/main/java/com/iemr/mmu/Application.java
+++ src/main/java/com/iemr/mmu/Application.java
@@ -22,22 +22,13 @@
 package com.iemr.mmu;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.iemr.mmu.utils.IEMRApplBeans;
 
-//@Configuration
-//@EnableAutoConfiguration
-//@ComponentScan
+
 @SpringBootApplication
-//@EnableCaching(proxyTargetClass = true)
-@EnableJpaRepositories
 public class Application {
 
 	public static void main(String[] args) {
