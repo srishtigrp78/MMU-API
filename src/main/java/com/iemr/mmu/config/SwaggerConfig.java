8d28f74a665ce6099412a4860a0968507bc03cdd
indraniBan <indraniBan@gmail.com>
2024-06-18 12:48:23 +0530

    swagger configuration

---
+++ src/main/java/com/iemr/mmu/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.iemr.mmu.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+	
+	@Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new 
+             Info().title("MMU API").version("version").description("A microservice for the creation and management of beneficiaries."))
+                .addSecurityItem(new SecurityRequirement().addList("my security"))
+                .components(new Components().addSecuritySchemes("my security",
+                        new SecurityScheme().name("my security").type(SecurityScheme.Type.HTTP).scheme("bearer")));
+    }
+
+}
