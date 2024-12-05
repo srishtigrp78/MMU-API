1c5b0de737f571d61edcb2ead3c93a330c2d5c9b
IN40068837 <IN40068837@L-156289031.wipro.com>
2024-12-05 15:26:10 +0530

    Formatting and dependency upgrade

--- src/main/java/com/iemr/mmu/utils/FilterConfig.java
+++ src/main/java/com/iemr/mmu/utils/FilterConfig.java
@@ -4,16 +4,16 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
 public class FilterConfig {
-	
+
 	@Bean
-    public FilterRegistrationBean<JwtUserIdValidationFilter> jwtUserIdValidationFilter(JwtAuthenticationUtil jwtAuthenticationUtil) {
-        FilterRegistrationBean<JwtUserIdValidationFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(new JwtUserIdValidationFilter(jwtAuthenticationUtil));
-        registrationBean.addUrlPatterns("/*");  // Apply filter to all API endpoints
-        return registrationBean;
-    }
+	public FilterRegistrationBean<JwtUserIdValidationFilter> jwtUserIdValidationFilter(
+			JwtAuthenticationUtil jwtAuthenticationUtil) {
+		FilterRegistrationBean<JwtUserIdValidationFilter> registrationBean = new FilterRegistrationBean<>();
+		registrationBean.setFilter(new JwtUserIdValidationFilter(jwtAuthenticationUtil));
+		registrationBean.addUrlPatterns("/*"); // Apply filter to all API endpoints
+		return registrationBean;
+	}
 
 }
