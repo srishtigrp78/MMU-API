f2124c93f284dc998dcab80758049fd6ba9edceb
IN40068837 <IN40068837@L-156289031.wipro.com>
2024-12-15 22:45:55 +0530

    fixed issue

--- src/main/java/com/iemr/mmu/data/login/Users.java
+++ src/main/java/com/iemr/mmu/data/login/Users.java
@@ -21,8 +21,6 @@
 */
 package com.iemr.mmu.data.login;
 
-import java.sql.Timestamp;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -31,60 +29,18 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
-import com.google.gson.annotations.Expose;
-
 @Entity
 @Data
 @Table(name = "m_user")
 public class Users {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Expose
 	@Column(name = "UserID")
 	private Long userID;
-	@Expose
-	@Column(name = "TitleID")	private Short titleID;
-	@Expose
-	@Column(name = "FirstName")
-	private String firstName;
-	@Expose
-	@Column(name = "MiddleName")
-	private String middleName;
-	@Expose
-	@Column(name = "lastName")
-	private String lastName;
-	@Expose
-	@Column(name = "GenderID")
-	private Short genderID;
-	@Expose
-	@Column(name = "MaritalStatusID")
-	private Short maritalStatusID;
-	@Expose
-	@Column(name = "DOB")
-	private Timestamp dob;
-	@Expose
-	@Column(name = "DOJ")
-	private Timestamp doj;
-	@Expose
-	@Column(name = "QualificationID")
-	private Integer qualificationID;
-	@Expose
+
 	@Column(name = "UserName")
 	private String userName;
-	@Expose
-	@Column(name = "Deleted")
+	@Column(name = "Deleted", insertable = false, updatable = true)
 	private Boolean deleted;
-	@Expose
-	@Column(name = "CreatedBy")
-	private String createdBy;
-	@Expose
-	@Column(name = "CreatedDate")
-	private Timestamp createdDate;
-	@Expose
-	@Column(name = "ModifiedBy")
-	private String modifiedBy;
-	@Expose
-	@Column(name = "LastModDate")
-	private Timestamp lastModDate;
 
 }
